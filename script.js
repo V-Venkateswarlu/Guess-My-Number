b9042676df40b3187008002f121877bb3912fb4c
v-venkateswarlu_Github <venkateswarluvutukuri32@gmail.com>
2023-12-24 21:29:13 +0530

    From Working Directory

--- script.js
+++ script.js
@@ -204,7 +204,6 @@ document.querySelector('.check').addEventListener('click', function(){
     //     }
     // }
 });
-
 document.querySelector('.again').addEventListener('click',function(){
     score=20;
     secretNumber=Math.trunc(Math.random()*20)+1;
